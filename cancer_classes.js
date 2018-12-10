c76a5bbf7399957b48964df1890e3815246c844a
Aryan Misra <aryanmisra4@gmail.com>
2018-12-09 19:33:07 -0500

    Add files via upload

---
+++ cancer_classes.js
@@ -0,0 +1,10 @@
+const CANCER_CLASSES ={
+	0: 'akiec, Actinic Keratoses (Solar Keratoses) or intraepithelial Carcinoma (Bowen’s disease)',
+  1: 'bcc, Basal Cell Carcinoma',
+  2: 'bkl, Benign Keratosis',
+  3: 'df, Dermatofibroma',
+  4: 'mel, Melanoma',
+  5: 'nv, Melanocytic Nevi',
+  6: 'vasc, Vascular skin lesion'
+
+}
